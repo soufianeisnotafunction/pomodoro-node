2c94a9f6a714cb9e79ac675e09b5276bf1484b37
Soufiane  Boutallaka <s.boutallaka@gmail.com>
2018-02-20 12:48:44 +0100

    last version

--- pomodoro2.0.js
+++ pomodoro2.0.js
@@ -1,31 +1,51 @@
-var say = require('say');
-var colors = require('colors');
-var workInSeconds = 1500;
-var pauseInSeconds = 300;
-var work = true;
-var msgWork = " WORK ! : ";
-var msgPause = "PAUSE ! : ";
-var time = workInSeconds;
-var msg = msgWork;
+const say = require("say");
+const figlet = require("figlet");
+const colors = require("colors");
+const readline = require("readline-sync");
 
-var talkWork = 'its time to work , stay focused' ;
-var talkPause = 'chill out , its time to relax' ;
-var talk = talkWork;
+let data = {
+  work: {
+    time: 1500,
+    msg: "WORK! : ",
+    talk: " its time to work , stay focused"
+  },
+  pause: {
+    time: 300,
+    msg: "PAUSE! : ",
+    talk: " chill out , its time to relax"
+  }
+};
+let work = true;
+let { time, msg, talk } = data.work;
 
+// PROMPT
+var readlineSync = require("readline-sync"),
+  timeRange = [25, 35, 45],
+  index = readlineSync.keyInSelect(timeRange, "Work time ?");
+time = timeRange[index] * 60;
 
-process.stdout.write("******** POMODORO ********\n".inverse);
-say.speak(talk , 'Alex' , 1);
+//CLEAR SCREEN
+process.stdout.write("\x1B[2J\x1B[0f");
+//POMODORO
+console.log(figlet.textSync("POMODORO", "Calvin S").red);
 
-var pomodoro = function () {
-if (time < 0){
-  work = !work;
-  msg = work ? msgWork : msgPause;
-  time = work ? workInSeconds : pauseInSeconds ;
-  talk = work ? talkWork : talkPause ;
-  say.speak(talk , 'Alex' , 1);
-}
-process.stdout.write( (msg + colors.red(Math.floor(time/60)) + "mn " + colors.red(Math.floor(time%60)) + "s left\r"));
-time--;
-}
+say.speak(talk, "Alex", 1);
 
-var countdownWork = setInterval(pomodoro , 1000);
+const pomodoro = () => {
+  if (time < 0) {
+    work = !work;
+    msg = work ? data.work.msg : data.pause.msg;
+    time = work ? data.work.time : data.pause.time;
+    talk = work ? data.work.talk : data.pause.talk;
+    say.speak(talk, "Alex", 1);
+  }
+  process.stdout.write(
+    `${msg} ${colors.red(Math.floor(time / 60))} mn  ${colors.red(
+      Math.floor(time % 60)
+    )} s left \r`
+  );
+  // process.stdout.write(figlet.textSync(Math.round(time%60) , 'Doh' ));
+  time--;
+};
+
+var timer = setInterval(pomodoro, 1000);
